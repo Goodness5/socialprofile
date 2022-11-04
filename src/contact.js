9762f58441ca79f37ba616431a5f85a9b4278fbf
goodness5 <goodnesskolapo@gmail.com>
2022-11-04 17:36:27 +0100

    all bugs killed

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,7 @@
 import React from "react";
 import './contact.css'
 import i4g from './i4g.png'
+// import './App.css'
 // import { Router, Route} from 'react-router'
 
 function Contact() {
@@ -14,10 +15,16 @@ function Contact() {
 
             <div class="form">
                 <form>
+                <div class="f1">
+                <div class="firstname">
                     <label for="last_name">First name</label>
                     <input id="first_name" type="text" placeholder="Enter your first name"></input>
-                    <label for="last_name">Last name</label>
+                    </div>
+                    <div class="lastname">
+                    <label for="last_name" class="lab">Last name</label>
                     <input id="last_name" type="text" placeholder="Enter your last name"></input>
+                    </div>
+                    </div>
                     <label for="email">Email</label>
                     <input id="email" type="mail" placeholder="yourname@example.com"></input>
                     
@@ -30,10 +37,10 @@ function Contact() {
                     </div>
                     <button type="submit" id="btn__submit">Send message</button>
                 </form>
-
-                <div id="footer"><p>zuri <span id="spa"></span> internship</p><img src={i4g} alt="" class="i4g"></img> </div>
+                </div>
+                <div id="footer"><p class="zuri">zuri <span id="spa"></span> internship</p><img src={i4g} alt="" class="i4g"></img> </div>
                 <div class="copy">HNG internship 9 Frontend Task</div>
-            </div>
+            
 </div>
     )
 }
